6fd990d3ec784ab549118828438cbef1c7a4f0de
isabelasantorini2548 <isabelasantorini2548@gmail.com>
2026-09-09 20:37:49 -0400

    fix: reset routes cache para HomeAcademico

--- app/admin/_layout.js
+++ app/admin/_layout.js
@@ -2,24 +2,6 @@ import { Stack } from 'expo-router';
 
 export default function AdminLayout() {
   return (
-    <Stack screenOptions={{headerShown: false}}>
-      {/* Rutas principales de admin */}
-      <Stack.Screen name="admin" options={{headerShown: false}}>
-        <Stack.Screen name="HomeAcademico" options={{headerShown: false}} />
-        <Stack.Screen name="ProyectoEvento" options={{headerShown: false}} />
-        <Stack.Screen name="EventosPendientes" options={{headerShown: false}} />
-        <Stack.Screen name="EventosAprobados" options={{headerShown: false}} />
-        <Stack.Screen name="EventosRechazados" options={{headerShown: false}} />
-        <Stack.Screen name="EventosCompletados" options={{headerShown: false}} />
-        <Stack.Screen name="EventosVencidos" options={{headerShown: false}} />
-        <Stack.Screen name=" editUser" options={{headerShown: false}} />
-        <Stack.Screen name="CrearUsuarioA" options={{headerShown: false}} />
-        <Stack.Screen name="reportes" options={{headerShown: false}} />
-      </Stack.Screen>
-
-      {/* Rutas adicionales */}
-      <Stack.Screen name="EventDetailScreen" options={{headerShown: false}} />
-      <Stack.Screen name="EventDetailComite" options={{headerShown: false}} />
-    </Stack>
+    <Stack screenOptions={{ headerShown: false }} />
   );
 }
